6d4a4cc0074c1356cabffd8c63644d3c57345d01
Dave Spina <dspina79@gmail.com>
2021-01-24 12:06:03 -0500

    Initial app setup

---
+++ AccessibilityApp/AccessibilityApp/AccessibilityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityAppApp.swift
+//  AccessibilityApp
+//
+//  Created by Dave Spina on 1/24/21.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
